2ad542a4f233678dd3018230d8c6e7da9964e7ba
Robert Klep <robert@klep.name>
2013-09-12 11:15:08 +0200

    Moved code around a bit

--- example.js
+++ example.js
@@ -1,20 +1,5 @@
 var Muxer = require('port-mux');
 
-Muxer()
-  // forward HTTP to port 3001
-  .addRule(/^(?:GET|POST|PUT|DELETE)\s/, 3001)
-  // forward HTTPS to port 3002
-  .addRule(/^\x16\x03[\x00-\x03]/, 3002)
-  // start listening; .listen() accepts the same arguments as net.Server#listen()
-  .listen(3000, function() {
-    var addr = this.address();
-    console.warn('Muxer listening on %s:%s', addr.address, addr.port);
-  })
-  // .listen() returns the net.Server instance, so we can attach event handlers
-  .on('connection', function(conn) {
-    console.log('%s - connection from %s', new Date(), conn.remoteAddress);
-  });
-
 // start HTTP server on port 3001
 require('http').createServer(function(req, res) {
   res.end('hello world from HTTP');
@@ -29,6 +14,22 @@ var options = {
 require('https').createServer(options, function(req, res) {
   res.end('hello world from HTTPS');
 }).listen(3002);
+
+// configure and start muxer
+Muxer()
+  // forward HTTP to port 3001
+  .addRule(/^(?:GET|POST|PUT|DELETE)\s/, 3001)
+  // forward HTTPS to port 3002
+  .addRule(/^\x16\x03[\x00-\x03]/, 3002)
+  // start listening; .listen() accepts the same arguments as net.Server#listen()
+  .listen(3000, function() {
+    var addr = this.address();
+    console.warn('Muxer listening on %s:%s', addr.address, addr.port);
+  })
+  // .listen() returns the net.Server instance, so we can attach event handlers
+  .on('connection', function(conn) {
+    console.log('%s - connection from %s', new Date(), conn.remoteAddress);
+  });
 
 // Check the results with curl:
 //
